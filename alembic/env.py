903c93a0385b70fc29f13adb975d5c5b21a9b558
sevegnps-eng <sevegnps@gmail.com>
2026-06-30 22:43:43 -0300

    Alterações na tabela pelo alembic

--- alembic/env.py
+++ alembic/env.py
@@ -23,17 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from models.appointments import Appointments, AppointmentStatus
-from models.car_make import Car_make
-from models.car_model import Car_model
-from models.city import City
-from models.color import Color
-from models.customer import Customer
-from models.employee import Employee
-from models.service import Service
-from models.state import State
-from models.user import User
-from models.vehicle import Vehicle
+import models
 
 from database.connection import Base
 target_metadata = Base.metadata
