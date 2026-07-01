8e9db944b044a18f24904d4a86ffe888ef43ea45
sevegnps-eng <sevegnps@gmail.com>
2026-06-30 22:57:10 -0300

    Include router na main para usar o Swagger

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from database.connection import Base, engine
-from models.appointments import Appointments, AppointmentStatus
-from models.car_make import Car_make
-from models.car_model import Car_model
-from models.city import City
-from models.color import Color
-from models.customer import Customer
-from models.employee import Employee
-from models.service import Service
-from models.state import State
-from models.user import User
-from models.vehicle import Vehicle
+from fastapi import FastAPI
+from routers.auth_router import auth_router
 
-Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+app.include_router(auth_router)
+
+
+
+
+
