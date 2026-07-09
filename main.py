964639f576e2bda595e73eeb3c4bf8418a166d56
sevegnps-eng <sevegnps@gmail.com>
2026-07-08 22:15:54 -0300

    Adicionado rota de users no main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from routers.auth_router import auth_router
+from routers.users_router import users_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
-
+app.include_router(users_router)
 
 
 
