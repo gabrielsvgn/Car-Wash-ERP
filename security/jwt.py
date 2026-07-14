0ab32a46a1a2dae28f1230f4f5084dd0db83e3be
sevegnps-eng <sevegnps@gmail.com>
2026-07-13 23:11:09 -0300

    Função para criar token finalizada

---
+++ security/jwt.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+import os
+from jose import jwt, JWTError
+
+load_dotenv()
+ACCESS_EXPIRED_TOKEN = int(os.getenv("ACCESS_EXPIRED_TOKEN"))
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+
+def create_token(user_id):
+    exp_date = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_EXPIRED_TOKEN)
+    dic_info = {"sub": user_id, "exp": exp_date}
+    jwt_encoded = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_encoded
