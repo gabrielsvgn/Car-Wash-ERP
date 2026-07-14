3b292f8e19b038388b79433c7882c861d5e7900d
sevegnps-eng <sevegnps@gmail.com>
2026-07-13 23:11:43 -0300

    Finalizada a primeira parte da rota de login

--- routers/auth_router.py
+++ routers/auth_router.py
@@ -4,6 +4,9 @@ from dependencies.session import get_session
 from security.password import bcrypt_context
 from schemas.user_schema import UserSchema
 from sqlalchemy.orm import Session
+from schemas.login_schema import LoginSchema
+from services.auth_service import auth_user
+from security.jwt import create_token
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -20,5 +23,17 @@ async def create_account(user_schema: UserSchema, session: Session = Depends(get
         new_user = User(name=user_schema.name, email=user_schema.email, contact=user_schema.contact, password=encrypted_password, tax_id=user_schema.tax_id, admin=user_schema.admin, idcity=user_schema.idcity, street=user_schema.street, house_number=user_schema.house_number)
         session.add(new_user)
         session.commit()
-        return {"message": "email successfully registered"}
-
+        return {"message": "user successfully registered"}
+    
+    
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session: Session = Depends(get_session)):
+    user = auth_user(login_schema.email, login_schema.password, session)
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found or invalid credentials. ")
+    else:
+        access_token = create_token(user.idusuario)
+        return {
+            "access_token": access_token,
+            "token_type": "Bearer"
+        }
