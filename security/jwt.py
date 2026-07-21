39d0b01a51b9fec6b2e67d3574631ff138651713
sevegnps-eng <sevegnps@gmail.com>
2026-07-20 23:32:26 -0300

    Atualização do user_id de dic_info para string

--- security/jwt.py
+++ security/jwt.py
@@ -9,8 +9,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 
 
-def create_token(user_id):
-    exp_date = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_EXPIRED_TOKEN)
-    dic_info = {"sub": user_id, "exp": exp_date}
+def create_token(user_id, token_duration = timedelta(minutes = ACCESS_EXPIRED_TOKEN)):
+    exp_date = datetime.now(timezone.utc) + token_duration
+    dic_info = {"sub": str(user_id), "exp": exp_date}
     jwt_encoded = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
     return jwt_encoded
