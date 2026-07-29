7c40d1ecbddcdb2d1bb56fd01d2c2dd85ca6bcd1
sevegnps-eng <sevegnps@gmail.com>
2026-07-28 21:26:44 -0300

    Include de model e make na main

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from routers.auth_router import auth_router
-from routers.users_router import users_router
+from routers.user_router import users_router
+from routers.appointments_router import appointments_router
+from routers.vehicle.vehicle_router import vehicle_router
+from routers.vehicle.make_router import make_router
+from routers.zip_code_router import zip_code
+from routers.vehicle.model_router import model_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(users_router)
-
+app.include_router(appointments_router)
+app.include_router(vehicle_router)
+app.include_router(zip_code)
+app.include_router(make_router)
+app.include_router(model_router)
 
 
 
