4e4b3c471e4032b76771c08e4ff699163a00dbaf
sevegnps-eng <sevegnps@gmail.com>
2026-07-28 21:14:25 -0300

    Criação do Schema e ResponseSchema de User

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
 class UserSchema(BaseModel):
@@ -12,5 +12,4 @@ class UserSchema(BaseModel):
     street: Optional[str] 
     house_number: Optional[int] 
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes = True)
