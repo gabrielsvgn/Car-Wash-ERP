c8c5080e03cc63773801ae80d139efe1d025675d
sevegn <sevegnps@gmail.com>
2026-08-16 14:38:10 -0300

    delete router

--- routers/vehicle/make_router.py
+++ routers/vehicle/make_router.py
@@ -8,21 +8,21 @@ from models.user import User
 
 make_router = APIRouter(prefix="/make", tags=["Make"])
 
-@make_router.get("/makes", response_model=list[MakeResponseSchema])
+@make_router.get("/list", response_model=list[MakeResponseSchema])
 async def list_car_makes(user: User = Depends(verify_token), session: Session = Depends(get_session)):
      makes = session.query(Car_make).all()
      if not makes:
           raise HTTPException(status_code=404, detail="The car's makes was not found")
      return makes
 
-@make_router.get("/makes/{idcar_make}", response_model=MakeResponseSchema)
+@make_router.get("/list/{idcar_make}", response_model=MakeResponseSchema)
 async def list_car_make_id(idcar_make: int, user: User = Depends(verify_token), session: Session = Depends(get_session)):
      make = session.query(Car_make).filter(Car_make.idcar_make==idcar_make).first()
      if not make:
           raise HTTPException(status_code=404, detail="The car's make was not found")
      return make
 
-@make_router.post("/makes")
+@make_router.post("/create")
 async def create_car_make(make_schema: MakeSchema, user: User = Depends(verify_token), session: Session = Depends(get_session)):
     car_make = session.query(Car_make).filter(Car_make.name==make_schema.name).first()
     if car_make:
@@ -32,7 +32,7 @@ async def create_car_make(make_schema: MakeSchema, user: User = Depends(verify_t
     session.commit()
     return {"message": "The car make was successfully created"}
 
-@make_router.put("/makes/{idcar_make}")
+@make_router.put("/update/{idcar_make}")
 async def update_car_make(idcar_make: int, name_schema: ChangeMakeNameSchema, user: User = Depends(verify_token), session: Session = Depends(get_session)):
      make = session.query(Car_make).filter(Car_make.idcar_make==idcar_make).first()
      if not make:
@@ -40,3 +40,12 @@ async def update_car_make(idcar_make: int, name_schema: ChangeMakeNameSchema, us
      make.name = name_schema.name
      session.commit()
      return {"message": "The make's name has been successffully changed"}
+
+@make_router.delete("/delete/{idcar_make}")
+async def delete_car_make(idcar_make: int, user: User = Depends(verify_token), session: Session = Depends(get_session)):
+     car_make = session.query(Car_make).filter(Car_make.idcar_make==idcar_make).first()
+     if not car_make:
+          raise HTTPException(status_code=404, detail="Car make was not found")
+     session.delete(car_make)
+     session.commit()
+     return {"message": "The make was successfully deleted"}
