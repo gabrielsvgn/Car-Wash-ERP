03a4e8b8db898cb78ef183328b3534adee769726
sevegn <sevegnps@gmail.com>
2026-08-30 16:40:53 -0300

    refactor(Vehicle): Correction HTTP Adress

--- routers/vehicle/vehicle_router.py
+++ routers/vehicle/vehicle_router.py
@@ -12,7 +12,7 @@ from models.vehicle import Vehicle
 
 vehicle_router = APIRouter(prefix="/vehicle", tags=["Vehicle"])
 
-@vehicle_router.post("/vehicles")
+@vehicle_router.post("")
 async def create_vehicle(vehicle_schema: VehicleSchema, user: User = Depends(verify_token), session: Session = Depends(get_session)):
      color = session.query(Color).filter(Color.idcolor==vehicle_schema.idcolor).first() 
      car_model = session.query(Car_model).filter(Car_model.idcar_model==vehicle_schema.idcar_model).first() 
@@ -31,7 +31,7 @@ async def create_vehicle(vehicle_schema: VehicleSchema, user: User = Depends(ver
      session.commit()
      return {"message": "The vehicle was successffully created"}
 
-@vehicle_router.get("/vehicles", response_model=list[VehicleResponseSchema])
+@vehicle_router.get("", response_model=list[VehicleResponseSchema])
 async def list_vehicles(user: User = Depends(verify_token), session: Session = Depends(get_session)):
     vehicles = session.query(Vehicle).all()
     return vehicles
